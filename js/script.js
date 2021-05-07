9dd59f473a511d43724e94ccd1653e4ddca6a554
DennisHelliarEdu <83814476+DennisHelliarEdu@users.noreply.github.com>
2021-05-07 11:42:29 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function sendMessageInq() {
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("userMessage").value;
+
+    alert("Your form has been submitted \n\nEmail: " + email + "\nMessage:\n" + message);
+}
